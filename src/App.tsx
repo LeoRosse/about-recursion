b7aad711ea709e411da136f6343d24eb8f42d9b3
Leonardo Rosseti <rosseti@extendi.it>
2021-12-22 14:14:54 +0100

    Remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,12 @@ import { RecursiveComponent } from './components/recursive-component';
 import './index.scss';
 import { store } from './store/store';
 
-const App = () => {
-  // const [policy, setPolicy] = React.useState<string>('private');
-  // window.console.log(policy, 'policy');
-  return (
-    <Provider store={store}>
-      <div className="mt-10 text-3xl mx-auto max-w-6xl">
-        {/* <div className="flex justify-between my-4">
-          <div>Name: {audience.containers[0].containerInfo.title}</div>
-          <div className="flex">
-            <button onClick={() => setPolicy('public')}>public</button>
-            <button onClick={() => setPolicy('private')}>private</button>
-          </div>
-        </div> */}
-        <RecursiveComponent containers={audience.containers} />
-      </div>
-    </Provider>
-  );
-};
+const App = () => (
+  <Provider store={store}>
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <RecursiveComponent containers={audience.containers} />
+    </div>
+  </Provider>
+);
+
 ReactDOM.render(<App />, document.getElementById('app'));
