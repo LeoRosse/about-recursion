3f333336c4b94aea7430f733c5d81ee273b60ae1
Leonardo Rosseti <rosseti@extendi.it>
2021-12-28 19:09:05 +0100

    Fix create-nested-object type buh

--- src/utility/create-nested-object.ts
+++ src/utility/create-nested-object.ts
@@ -7,10 +7,8 @@ import { isContainer } from '../types/is-container';
 const createObjectNested = (element: Container | Chart): DataSet => {
   if (isContainer(element)) {
     return {
-      [element.containerInfo.id]: element.containerInfo?.children?.reduce(
-        (acc, curr) => ({ ...acc, ...createObjectNested(curr) }),
-        {},
-      ),
+      [element.containerInfo.id]:
+        element.containerInfo?.children?.reduce((acc, curr) => ({ ...acc, ...createObjectNested(curr) }), {}) || {},
     };
   }
   return { [element.chartInfo.id]: { [DATA_KEY]: {} } };
